b7cfab8a545f8dcd1dd519857ce046b91ad503b8
bjedrzejewski <bjedrzejewski@scottlogic.com>
2015-10-08 16:43:36 +0100

    Cleanup of comments

--- app/transaction/transaction-controller.js
+++ app/transaction/transaction-controller.js
@@ -47,31 +47,6 @@ angular.module('bitcoinTransactionControllers', ['txDirectives']).controller('Tr
         }
         asyncExpand(0);
 
-        //for (var i = 0; i < that.uNodes.length; i++) {
-        //    //coinbase existing means the end, so should stop looking
-        //    if (that.uNodes[i].vin && !that.uNodes[i].vin[0].coinbase) {
-        //        //iterating via transaction
-        //        for (var j = 0; j < that.uNodes[i].vin.length; j++) {
-        //            //I need immediate function to create a closure and get i and j stored
-        //            (function () {
-        //                var _i = i;
-        //                var _j = j;
-        //                TxData.get({hashValue: that.uNodes[_i].vin[_j].txid}, function (newTx) {
-        //                    var thisNumber = that.nodes.length;
-        //                    var originNumber = that.uNodes[_i].oNumber;
-        //                    newTx.oNumber = thisNumber;
-        //                    that.links.push({'target': newTx.oNumber, 'source': that.uNodes[_i].oNumber});
-        //                    that.nodes.push(newTx);
-        //                    that.newUNodes.push(newTx);
-        //
-        //
-        //                }, function (errorResult) {
-        //                    console.log('error: ' + errorResult);
-        //                });
-        //            }());
-        //        }
-        //    }
-        //}
     }
 
     if (that.rparam.length === 64) {
