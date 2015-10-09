0a173bc086ab1ee84ee8c931fecc6ff17e5052e6
bjedrzejewski <bjedrzejewski@scottlogic.com>
2015-10-09 11:05:29 +0100

    Added auto expand

--- app/transaction/transaction-controller.js
+++ app/transaction/transaction-controller.js
@@ -17,7 +17,7 @@ angular.module('bitcoinTransactionControllers', ['txDirectives']).controller('Tr
     //function that expands and updates nodes
     that.expandNodes = function () {
         //will work because js is single threaded
-        if(that.expanding){
+        if (that.expanding) {
             return;
         }
         that.expanding = true;
@@ -27,42 +27,67 @@ angular.module('bitcoinTransactionControllers', ['txDirectives']).controller('Tr
         function asyncExpand(i1) {
             if (i1 >= that.uNodes.length) {
                 that.expanding = false;
+                //finishes
                 return;
             } else {
-                function asyncExpandInner(i2, j) {
-                    if (j >= that.uNodes[i2].vin.length) {
-                        asyncExpand(++i2, 0);
-                    }
-                    else {
-                        if (that.uNodes[i2].vin[j].txid)
-                            TxData.get({hashValue: that.uNodes[i2].vin[j].txid}, function (newTx) {
-                                var thisNumber = that.nodes.length;
-                                var originNumber = that.uNodes[i2].oNumber;
-                                that.uNodes[i2].expanded = true;
-                                newTx.oNumber = thisNumber;
-                                newTx.x = that.uNodes[i2].x;
-                                newTx.y = that.uNodes[i2].y;
-                                that.links.push({'target': newTx.oNumber, 'source': that.uNodes[i2].oNumber});
-                                that.nodes.push(newTx);
-                                that.newUNodes.push(newTx);
-                                asyncExpandInner(i2, ++j);
-                            }, function (errorResult) {
-                                console.log('error: ' + errorResult);
-                            });
-                        else {
-                            console.log('bingo ended');
-                            that.uNodes[i2].ended = true;
-                            asyncExpandInner(i2, ++j);
-                        }
-                    }
-                }
-
-                asyncExpandInner(i1, 0);
+                expandNode(i1, 0, asyncExpand);
             }
         }
 
         asyncExpand(0);
+    }
 
+    that.autoExpand = function () {
+        //will work because js is single threaded
+        if (that.expanding) {
+            return;
+        }
+        that.expanding = true;
+        that.uNodes = that.newUNodes;
+        that.newUNodes = [];
+
+        function asyncExpandForever(i1) {
+            if (i1 >= that.uNodes.length) {
+                that.expanding = false;
+                //runs forever
+                that.autoExpand();
+            } else {
+                expandNode(i1, 0, asyncExpandForever);
+            }
+        }
+
+        asyncExpandForever(0);
+    }
+
+    function expandNode(i2, j, callback) {
+        function asyncExpandInner(i2, j) {
+            if (j >= that.uNodes[i2].vin.length) {
+                callback(++i2);
+            }
+            else {
+                if (that.uNodes[i2].vin[j].txid)
+                    TxData.get({hashValue: that.uNodes[i2].vin[j].txid}, function (newTx) {
+                        var thisNumber = that.nodes.length;
+                        var originNumber = that.uNodes[i2].oNumber;
+                        that.uNodes[i2].expanded = true;
+                        newTx.oNumber = thisNumber;
+                        newTx.x = that.uNodes[i2].x;
+                        newTx.y = that.uNodes[i2].y;
+                        that.links.push({'target': newTx.oNumber, 'source': that.uNodes[i2].oNumber});
+                        that.nodes.push(newTx);
+                        that.newUNodes.push(newTx);
+                        asyncExpandInner(i2, ++j);
+                    }, function (errorResult) {
+                        console.log('error: ' + errorResult);
+                    });
+                else {
+                    console.log('bingo ended');
+                    that.uNodes[i2].ended = true;
+                    asyncExpandInner(i2, ++j);
+                }
+            }
+        }
+        asyncExpandInner(i2, 0);
     }
 
     if (that.rparam.length === 64) {
